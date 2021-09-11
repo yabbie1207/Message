a95c88ec4e28a675d979af698a211769cbdace4b
yabbie1207 <77682314+yabbie1207@users.noreply.github.com>
2021-09-11 21:00:00 +0800

    Create board.js

---
+++ board.js
@@ -0,0 +1,7 @@
+<script src="https://apis.google.com/js/plusone.js">{lang: 'zh-TW'}</script>
+<g:comments
+href="https://yabbie1207.github.io/Message/board.js"
+width="680"
+first_party_property="BLOGGER"
+view_type="FILTERED_POSTMOD">
+</g:comments>
